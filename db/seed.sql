7e995b0f25c138ab1f9129cd821d080e9d1f72c6
Doug Ritchie <dritchie3@sbcglobal.net>
2020-04-16 17:53:37 -0500

    created all directories

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM eat_burgerdb.burgers
+LIMIT 0, 1000
+
+-- Date: 2020-04-16 17:39
+*/
+INSERT INTO `` (`id`,`burger`,`devoured`) VALUES (1,'Cheeseburger',NULL);
+INSERT INTO `` (`id`,`burger`,`devoured`) VALUES (2,'hamburger',NULL);
+INSERT INTO `` (`id`,`burger`,`devoured`) VALUES (3,'happy meal Burger',1);
--- server.js
+++ server.js
@@ -17,53 +17,82 @@ app.set("view engine", "handlebars");
 
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "!123Bobby123",
-    database: "eat_burgerDB"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "!123Bobby123",
+  database: "eat_burgerDB"
+});
 
-  // Initiate MySQL Connection.
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  // Serve index.handlebars to the root route, populated with all quote data.
-  app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burgers;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
-  
-      res.render("index", { burgers: data });
-    });
-  });
-//Delete burger from database
-app.delete("/api/burgers/:id", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
+
+
+
+
+
+//How do I find the data I need (explain the syntax to seperate the different columns.).  Why can I not use 2 res.render() statements
+
+
+
+
+
+
+
+
+    console.log({ id: data});
     if (err) {
-      // If an error occurred, send a generic server failure
       return res.status(500).end();
     }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
+    res.render("index", { burgers: data});
+    
+  });  
 
+  //     res.render("index", { devoured: data});
+  
+  });  
+//Add Post to add new Burger name
+app.post("/api/burgers", function(req, res) {
+  connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burger], function(err, res) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
   });
 });
 
+//Add update to add a 1 to the my sequel table and move the burger to devoured.
 
 
+
+app.put("/api/burgers/:devoured", function(req, res) {
+  connection.query(
+    "UPDATE burgers SET devoured = 1 WHERE devoured = null ",
+    [req.body.burgers, req.params.devoured],
+    function(err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
