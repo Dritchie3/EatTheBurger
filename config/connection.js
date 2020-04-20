7560e784137dc36cd851fe7f8379e430adc7e646
Doug Ritchie <dritchie3@sbcglobal.net>
2020-04-20 11:36:26 -0500

    add JAWSDB credentials

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql/createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+    host: "	lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "!123Bobby123",
-    database: "eat_burgerDB"
-});
+    user: "	ezsmoybrl5g5fmes",
+    password: "z4e5bi3embuj4sza",
+    database: "	v3ljip0j1brttkxo"
+    });
+};
 
 // Initiate MySQL Connection.
 connection.connect(function (err) {
