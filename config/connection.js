110ace2ecefe760c5c70300c16dd8c2b7e13403a
Doug Ritchie <dritchie3@sbcglobal.net>
2020-04-27 10:34:49 -0500

    update password corrction

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,3 @@
-//connection for "localhost"
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "eat_burgerDB"
-});
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
-
 //connection for "Heroku"
 var mysql = require("mysql");
 
