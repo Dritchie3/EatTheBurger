63cb7efa23811813bf84575117acbbff70752d54
Doug Ritchie <dritchie3@sbcglobal.net>
2020-04-27 11:31:34 -0500

    update password 7

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,15 @@
-//connection for "localhost"
-const mysql = require("mysql");
+//connection for "Heroku"
+var mysql = require("mysql");
 
-
-require("dotenv").config();
-const api_Key = process.env.MYSQL_API_KEY;
-// console.log(api_Key);
-
-
-const connection = mysql.createConnection({
-    host: "localhost",
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+    host: "lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: api_Key,
-    database: "eat_burgerDB"
-});
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+    user: "ezsmoybrl5g5fmes",
+    password: "ga0tjiutl78bv5it",
+    database: "	v3ljip0j1brttkxo"
+    });
+};
